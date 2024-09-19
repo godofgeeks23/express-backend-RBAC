cc0d23ece00dbe3c9e01a0b30d21deeef3d7c4f6
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-09-19 23:09:21 +0530

    Create sample.model.js

---
+++ models/sample.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// This is creating a new schema for the product model.
+const sampleSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Sample", sampleSchema);
