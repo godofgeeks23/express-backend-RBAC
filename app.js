3c5885979689f2c7bbd598d2b2c58f26d65e5e83
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-09-27 17:20:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express();
 // middleware to parse the body of the request and make it available in the req.body object
 app.use(express.json());
 
-// root route - to check if the server is running.
+// root route - to check if the server is running
 app.get("/", (req, res) => {
   res.status(200).json({ alive: "True" });
 });
 
-// Telling the server to use the routes in the SampleRoutes file.
+// Telling the server to use the routes in the SampleRoutes file
 app.use("/api", SampleRoutes);
 
 module.exports = app;
