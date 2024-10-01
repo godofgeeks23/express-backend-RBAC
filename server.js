ccabf7aae97c4ab662aafc118ff691ff0f174f6d
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-01 22:26:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 require("dotenv").config();
 
-// Connect to the db and then starting the API web server
+// connect to the db and then starting the API web server
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
