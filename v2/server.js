f704bc4b8d41f628bfa742cd1c692f2c6c66cb44
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-08 21:53:10 +0530

    Create server.js

---
+++ v2/server.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const PORT = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+// connect to the db and then starting the API web server
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(
+      PORT,
+      console.log("Connected to DB and Server started on port 5000")
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
