d862419ff2ea5ac9194449396a674e99557007b4
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-11 19:56:50 +0530

    Update server.test.js

--- v1/tests/server.test.js
+++ v1/tests/server.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("GET /", () => {
-  it("Should respond with code 200", async () => {
+  it("Should respond with statud code 200", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
   });
