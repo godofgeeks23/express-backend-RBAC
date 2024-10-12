1868f15e5ad9f250fc086a168492d61e6c85821c
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-12 22:12:38 +0530

    Update app.js

--- v2/app.js
+++ v2/app.js
@@ -5,7 +5,7 @@ const app = express();
 // middleware to parse the body of the request and make it available in the req.body object
 app.use(express.json());
 
-// root route - to check if the server is running
+// root route - to check if the server is up
 app.get("/", (req, res) => {
   res.status(200).json({ alive: "True" });
 });
